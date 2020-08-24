a82d413e4dbe444fc93ae6b28a1edc93b22d187f
alice <alice@alicereuter.com>
2020-08-23 20:23:56 -0500

    append to log file instead of overwriting

--- src/main/java/com/example/examplemod/DeathEventHandler.java
+++ src/main/java/com/example/examplemod/DeathEventHandler.java
@@ -3,6 +3,7 @@ package com.example.examplemod;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 import net.minecraftforge.fml.common.eventhandler.EventPriority;
 
+import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
@@ -15,10 +16,11 @@ public class DeathEventHandler {
 	@SubscribeEvent(priority = EventPriority.NORMAL)
 	public void LivingDeathEvent(LivingDeathEvent e) {
 		try {
-			FileWriter myfile = new FileWriter("/home/alice/died.txt");
+			FileWriter myfile = new FileWriter("/home/alice/died.txt", true);
+			BufferedWriter bw = new BufferedWriter(myfile);
 			EntityLivingBase entity = e.getEntityLiving();
-			myfile.write(e.getSource().getDeathMessage(entity).getUnformattedComponentText());
-			myfile.close();
+			bw.append(e.getSource().getDeathMessage(entity).getUnformattedComponentText() + "\n");
+			bw.close();
 
 		} catch (IOException e1) {
 			// TODO Auto-generated catch block
