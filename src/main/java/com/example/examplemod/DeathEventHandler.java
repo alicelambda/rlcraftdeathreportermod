494eba5646189d649927eb6fdaf51dc9a8666805
alice <alice@alicereuter.com>
2020-08-25 14:16:52 -0500

    hooked player entity to get death score also prevented appending if entity dying is not the player

--- src/main/java/com/example/examplemod/DeathEventHandler.java
+++ src/main/java/com/example/examplemod/DeathEventHandler.java
@@ -8,25 +8,28 @@ import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
 
+import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.entity.EntityLivingBase;
 import net.minecraftforge.event.entity.living.LivingDeathEvent;
 
 public class DeathEventHandler {
-	
+
 	@SubscribeEvent(priority = EventPriority.NORMAL)
 	public void LivingDeathEvent(LivingDeathEvent e) {
-		try {
-			FileWriter myfile = new FileWriter("/home/alice/died.txt", true);
-			BufferedWriter bw = new BufferedWriter(myfile);
-			EntityLivingBase entity = e.getEntityLiving();
-			bw.write(e.getSource().getDeathMessage(entity).getUnformattedComponentText() + "\n");
-			bw.close();
-			myfile.close();
-		} catch (IOException e1) {
-			// TODO Auto-generated catch block
-			e1.printStackTrace();
+		EntityLivingBase entity = e.getEntityLiving();
+		if (entity instanceof EntityPlayer) {
+			EntityPlayer player = (EntityPlayer) entity;
+			try {
+				FileWriter myfile = new FileWriter("/home/alice/died.txt", true);
+				BufferedWriter bw = new BufferedWriter(myfile);
+				bw.write(e.getSource().getDeathMessage(entity).getUnformattedComponentText() + "," + player.getScore() + "\n");
+				bw.close();
+				myfile.close();
+			} catch (IOException e1) {
+				// TODO Auto-generated catch block
+				e1.printStackTrace();
+			}
 		}
-		
 	}
 
 }
